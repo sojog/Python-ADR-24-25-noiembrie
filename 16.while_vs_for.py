c9c072905dd04d42b4234459cb803c1514ddf7b9
Silviu Ojog <sil.ojog@gmail.com>
2025-11-24 12:46:30 +0200

    Colectii de date

---
+++ 16.while_vs_for.py
@@ -0,0 +1,18 @@
+
+count = 1
+# Bucla infinita
+while count < 4:
+    print("count", count)
+    count += 1
+
+print("----")
+# Bucla finita
+for i in range(3):
+     print("count", i, type(i))
+
+## In alte limbaje
+## for (int i = 0; i < 3; i++) -> nu functioneaza in Python
+
+
+
+
