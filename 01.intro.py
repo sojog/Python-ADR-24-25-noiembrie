2d836129e52590434978d4bc403bcc545fcd906c
Silviu Ojog <sil.ojog@gmail.com>
2025-11-24 10:11:45 +0200

    Intro

---
+++ 01.intro.py
@@ -0,0 +1,19 @@
+
+# CTRL + /      sau     CMD + /
+# Acesta este un comentariu
+
+print("Hello, World!?? ")
+print('Hello, World!?? ')
+
+x = "Hello, World!?? "
+print(x, type(x))
+
+
+print("""Hello,
+      
+       World!?? """)
+print('''Hello,
+      
+      
+       World!?? ''')
+
---
+++ 02.variabile.py
@@ -0,0 +1,17 @@
+
+## Python este un limbaj loosly typed, adica variabilelor nu trebuie sa li se precizeze tipul de date
+
+## Primitive -> tipuri de date de baza
+
+x = True # boolean (bool)
+print(x, type(x))
+
+x = 10 # integer (int)
+print(x, type(x))
+
+x = 10.34 # floating number (float)
+print(x, type(x))
+
+x = "10" # string (str)
+print(x, type(x))
+
